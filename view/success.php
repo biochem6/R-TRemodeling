b021c7c4b6fc931484c1b035dc4fe4d2c5b70554
Andrew <aashland5@gmail.com>
2019-02-03 22:28:34 -0500

    .

--- view/success.php
+++ view/success.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" type="text/css" href="../css/view.css">
-    <style><?php include '../css/view.css'; ?> </style>
+    <style><?php include_once("../css/view.css"); ?> </style>
 </head>
 <body>
 <main>
